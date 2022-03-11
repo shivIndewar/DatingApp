a32138e78ffc8ea640030eee8876b7f8e03ce9dc
Shivprasad Indewar <indewars@gmail.com>
2022-03-11 20:46:29 +0530

    Applied fix

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 import { Group } from '../_models/group';
 import { Message } from '../_models/message';
 import { user } from '../_models/user';
+import { BusyService } from './busy.service';
 import { getPaginatedResult, getPaginationHeaders } from './paginationHelper';
 
 @Injectable({
@@ -22,9 +23,10 @@ export class MessageService {
  private messageThreadSource = new BehaviorSubject<Message[]>([]);
  messageThread$ = this.messageThreadSource.asObservable();
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient, private busyService : BusyService) { }
 
   createHubConnection(user : user, otherUsername : string){
+    this.busyService.busy();
     this.hubConnection = new HubConnectionBuilder()
         .withUrl(this.hubUrl + 'message?user=' + otherUsername, {
           accessTokenFactory :  () => user.token
@@ -32,7 +34,9 @@ export class MessageService {
         .withAutomaticReconnect()
         .build();
 
-        this.hubConnection.start().catch(error => console.log(error));
+        this.hubConnection.start()
+        .catch(error => console.log(error))
+        .finally(() => this.busyService.idle());
         this.hubConnection.on("ReceiveMessageThread", messages =>{
           this.messageThreadSource.next(messages);
         })
@@ -59,6 +63,7 @@ export class MessageService {
 
   stopHubConnection(){
     if(this.hubConnection){
+      this.messageThreadSource.next([]);
       this.hubConnection.stop();
     }
   }
